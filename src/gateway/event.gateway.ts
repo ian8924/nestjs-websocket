79f1e26280593fa729df58976336fc5729dd3d14
ian <ian.l@nexusplay.tw>
2024-10-01 14:04:20 +0800

    update

--- src/gateway/event.gateway.ts
+++ src/gateway/event.gateway.ts
@@ -1,17 +1,25 @@
 import {
-  ConnectedSocket,
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server } from 'socket.io';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class EventGateway {
+  @WebSocketServer()
+  server: Server;
   // 訂閱 channel 名稱
-  @SubscribeMessage('newMessage')
-  handleMessage(@MessageBody() body: any, @ConnectedSocket() client: Socket) {
-    client.emit('onMessage');
-    console.log(body, 'newMessage');
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    this.server.emit('message', message);
   }
+
+  //   @SubscribeMessage('time')
+  //   handleTime(@MessageBody() body: any, @ConnectedSocket() client: Socket) {
+  //     const time = new Date().getTime();
+  //     this.server.emit('time', time);
+  //     this.server.emit('message', `Server time: ${time}`);
+  //   }
 }
